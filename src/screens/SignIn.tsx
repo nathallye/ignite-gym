e88340c394dbc98d7f2c53854ba768caf2b45ed2
Nathallye Bacelar <nathallyet@gmail.com>
2023-09-08 23:56:52 -0300

    React native svg transformer

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,25 @@
-import { VStack, Image } from "native-base";
+import { VStack, Image, Text, Center } from "native-base";
 
+import LogoSvg from "../assets/img/logo.svg";
 import BackgroundImg from "../assets/img/background.png";
 
 export const SignIn = () => {
   return (
     <VStack flex={1} bg="gray.700">
-      <Image 
+      <Image
         source={BackgroundImg}
         alt="Pessoas treinando"
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo.
+        </Text>
+      </Center>
     </VStack>
   );
-}
+};
