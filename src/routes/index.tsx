05b9314eb6576e7fd4a7764d08c3505878e4f112
Nathallye Bacelar <nathallyet@gmail.com>
2023-09-10 22:09:44 -0300

    Modifying the navigation theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,20 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 
 import { AuthRoutes } from "./auth.routes";
 
+import { Box, useTheme } from "native-base";
+
 export const Routes = () => {
+  const { colors } = useTheme();
+
+  const theme = DefaultTheme;
+  theme.colors.background = colors.gray[700];
+
   return (
-    <NavigationContainer>
-      <AuthRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="gray.700">
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   );
-}
+};
