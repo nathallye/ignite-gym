1df4183f27861c66de1c1ffc042ba11b6cd61818
Nathallye Bacelar <nathallyet@gmail.com>
2023-09-11 16:01:15 -0300

    Creating the structures of the App interfaces

---
+++ src/screens/Exersice.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export const Exercise = () => {
+  return (
+    <Center flex={1}>
+      <Text>Exercise</Text>
+    </Center>
+  );
+};
---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base';
+
+export const Home = () => {
+  return (
+    <Center flex={1}>
+      <Text>Home</Text>
+    </Center>
+  );
+};
