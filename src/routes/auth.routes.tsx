5a3cbac40760abcd47b1305f167c39c2105dd48b
Nathallye Bacelar <nathallyet@gmail.com>
2023-09-10 21:58:20 -0300

    Removing the Header - headerShown

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -14,7 +14,7 @@ const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
 
 export const AuthRoutes = () => {
   return (
-    <Navigator>
+    <Navigator screenOptions={{ headerShown: false }}>
       <Screen
         name="signIn"
         component={SignIn}
