cb6e931a73e77d6e0bb98a6db5b5ad3a8db5308c
Nathallye Bacelar <nathallyet@gmail.com>
2023-09-11 16:46:46 -0300

    Styling the Bottom Navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,4 @@
+import { Platform } from "react-native";
 import { useTheme } from "native-base";
 import { BottomTabNavigationProp, createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -31,7 +32,14 @@ export const AppRoutes = () => {
       headerShown: false,
       tabBarShowLabel: false,
       tabBarActiveTintColor: colors.green[500],
-      tabBarInactiveTintColor: colors.gray[200]
+      tabBarInactiveTintColor: colors.gray[200],
+      tabBarStyle: {
+        backgroundColor: colors.gray[600],
+        borderTopWidth: 0,
+        height: Platform.OS === "android" ? 'auto' : 96,
+        paddingBottom: sizes[10],
+        paddingTop: sizes[6]
+      }
     }}>
       <Screen
         name="home"
@@ -66,6 +74,7 @@ export const AppRoutes = () => {
       <Screen
         name="exercise"
         component={Exercise}
+        options={{ tabBarButton: () => null }}
       />
     </Navigator>
   );
