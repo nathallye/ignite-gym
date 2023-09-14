c27d97317876561adebf6421259804d5f9acbc95
Nathallye Bacelar <nathallyet@gmail.com>
2023-09-14 01:58:06 -0300

    UserPhoto component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,10 +1,17 @@
 import { Heading, HStack, Text, VStack } from "native-base";
 
-import { UserPhoto } from "./UsePhoto";
+import { UserPhoto } from "./UserPhoto";
 
 export const HomeHeader = () => {
   return (
     <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={{ uri: 'https://github.com/nathallye.png' }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={4}
+      />
+
       <VStack>
         <Text color="gray.100" fontSize="md">
           Olá,
@@ -13,13 +20,6 @@ export const HomeHeader = () => {
         <Heading color="gray.100" fontSize="md">
           Nathallye
         </Heading>
-
-        <UserPhoto
-          source={{ uri: 'https://github.com/nathallye.png' }}
-          size={16}
-          alt="Imagem do usuário"
-          mr={4}
-        />
       </VStack>
     </HStack>
   );
